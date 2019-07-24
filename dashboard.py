f22979fbf462634b6594edc417ba6dcb7862f378
Mario Damiano Russo <russomariod@gmail.com>
2019-07-24 23:06:04 +0100

    a

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,6 @@ import pandas as pd
 
 # Load the iris dataset
 df = pd.read_csv("https://raw.githubusercontent.com/RussoMarioDamiano/DashTutorial/master/iris.csv?")
-df.head()
 
 # Download a CSS template from the internet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -15,7 +14,15 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # Define a layout for our app
 app.layout = html.Div([
-    html.Div("Hello, World!")
+    dcc.Graph(
+        figure={
+            "data":[{"x":df[df.species == specie].loc[:, "sepal_length"],
+                     "y":df[df.species == specie].loc[:, "sepal_width"],
+                     "name": specie,
+                     "type": "scatter",
+                     "mode": "markers"} for specie in df.species.unique()]
+            }
+        )
 ])
 
 if __name__ == "__main__":
